c71aeeb3f9937c79a5f6d168dd57706e83b7bff7
Amit Kumar <Amit Kumar@THEBOURNEGENIUS>
2016-07-16 12:46:34 +0530

    Add project files.

---
+++ Csharp_tutorials/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_tutorials
+{
+
+    class Mathfuncs
+    {
+        public static int Add(int num1, int num2)
+        {
+
+            return num1 + num2;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello");
+            Console.WriteLine(Mathfuncs.Add(1, 2));
+            Console.ReadKey();
+            Console.WriteLine("Hello again!");
+        }
+    }
+}
